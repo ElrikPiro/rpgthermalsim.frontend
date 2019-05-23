eddd3c0d99ebcb2f48dd76394a80ec9551563fff
David Baselga <baselga.david@gmail.com>
2019-05-23 14:21:31 +0200

    Advancing with first pane implementation
    Heatmap colors must be fixed

--- src/site/src/index.js
+++ src/site/src/index.js
@@ -46,9 +46,132 @@ class Building extends React.Component {
 	
 	render() {
 		const status = this.state.apidata;
+		let rom = "";
+		if(status.rooms){
+			rom = status.rooms.map((data,room) => {
+				return (
+					<div>
+						<Room data={data} />
+					<br />
+					</div>
+				);
+			});
+		}
 		
     return (
-      <div>BUILDING OUTPUT: {status}</div>
+      <div>ITERATIONS: {status.iteration}<br />{rom}</div>
+    );
+  }
+}
+
+class Room extends React.Component {
+    constructor(props) {
+    	super(props);
+    	this.state = {
+    			data: props.data,
+    	};
+    }
+	
+	render() {
+		const status = this.state.data;
+		
+		let grid = [];
+		for(let i = (status.h-1) ; i >= 0 ; i--){
+			for(let j = 0 ; j < status.w ; j++){
+				grid.push(<Cell data={status.cells[i*status.w+j]} room={status.id} cellid={i*status.w+j} />);
+			}
+			grid.push(<br />);
+			grid.push(<br />);
+		}
+		
+		return (
+				<div>
+				{status.id} : {status.desc} <br /> {grid}
+				</div>
+		);
+  }
+}
+
+class Cell extends React.Component {
+    constructor(props) {
+    	super(props);
+    	this.state = {
+    			data: props.data,
+    			room: props.room,
+    			cellid: props.cellid,
+    	};
+    }
+	
+	render() {
+		const temp = this.state.data.temp_counters;
+		let background = "000";
+		let text = null;
+		
+		/*
+		 * if(this.temp_counters<=-273) lerp = 0;
+		else if(this.temp_counters > 500) lerp = LERP.length-1;
+		else {
+			float tempmap = (float) (this.temp_counters+273)/773;
+			lerp = (int) Math.floor(tempmap*(LERP.length-1));
+		}
+		 */
+		//29 colors
+		let lerp;
+		let colormap = [
+			"#0039AD",
+			"#004DAC",
+			"#0062AC",
+			"#0077AB",
+			"#008CAB",
+			"#00A1AB",
+			"#00AAA0",
+			"#00AA8B",
+			"#00AA76",
+			"#00A961",
+			"#00A94C",
+			"#00A937",
+			"#00A822",
+			"#00A80D",
+			"#06A800",
+			"#1BA700",
+			"#2FA700",
+			"#43A700",
+			"#58A600",
+			"#6CA600",
+			"#80A600",
+			"#94A500",
+			"#A5A200",
+			"#A58E00",
+			"#A47900",
+			"#A46500",
+			"#A45100",
+			"#A33D00",
+			"#A32800",
+			"#A21500",
+		]
+		if(temp <= -273) lerp = 0;
+		else if(temp > 500) lerp = colormap.length-1;
+		else{
+			let tempmap = (temp+273)/773;
+			lerp = Math.floor(tempmap*(colormap.length-1));
+			console.log("tempmap:" + tempmap);
+		}
+		
+		background = colormap[lerp];
+		console.log("temp:" + temp);
+		console.log("lerp:" + lerp);
+		console.log("bg:" + background);
+		
+    return (
+    	<span>
+    	<button 
+	    className="cell" 
+	    onClick={() => this.props.onClick()}
+    	style={{background: background}}
+      >
+	    {Math.floor(temp)}
+      </button>
+    	</span>
     );
   }
 }
@@ -63,3 +186,4 @@ ReactDOM.render(
   </div>,
   document.getElementById('root')
 );
+
